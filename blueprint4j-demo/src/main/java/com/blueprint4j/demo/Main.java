d4f8683df2a7d6d61085aefb2e6a995e3b92bb26
Jan <jan@blueprint4j.com>
2014-06-30 22:49:03 +0200

    No comment

--- blueprint4j-demo/src/main/java/com/blueprint4j/demo/Main.java
+++ blueprint4j-demo/src/main/java/com/blueprint4j/demo/Main.java
@@ -1,31 +1,64 @@
 package com.blueprint4j.demo;
 
-import com.blueprint4j.core.doc.ApplicationDocument;
 import com.blueprint4j.core.doc.ApplicationDocumentList;
 import com.blueprint4j.core.doc.DocumentGeneratorList;
 import com.blueprint4j.core.doc.DocumentationSet;
 import com.blueprint4j.core.doc.html.HTMLDocumentGenerator;
 import com.blueprint4j.core.translate.BasicTranslator;
 import com.blueprint4j.core.translate.TranslatorList;
-import com.blueprint4j.template.servers.ServerDiagram;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Properties;
+
 
 /**
  *
  */
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+    /**
+     * Detects the client's operating system.
+     */
+    protected final static String osName = System.getProperty("os.name")
+            .replaceAll("\\s", "");
+
+    /**
+     * Load the properties file.
+     */
+    protected final static String cfgProp = "blueprint4j.properties";
+    protected final static Properties configFile = new Properties() {
+        {
+            try {
+                load(new FileInputStream(cfgProp));
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+    };
+
+    /**
+     * The directory where temporary files will be created.
+     */
+    public static String outputDirectory = configFile
+            .getProperty("outputDirFor" + osName);
+
+    /**
+     * Where is your dot program located? It will be called externally.
+     */
+    public static String DOT = configFile.getProperty("dotFor" + osName);
+
+
+    public static void main(String[] args) throws IOException {
 
 		/* Define output directory*/
-        File outputDir = new File(System.getProperty("user.dir"));
+        //File outputDir = new File(System.getProperty("user.dir"));
 
+        File outputDir = new File(outputDirectory);
         //TODO rename to Project? project = set of translatable documents
         /* Define project */
-        DocumentationSet documentationSet = new DocumentationSet("demo", outputDir) {
+        DocumentationSet documentationSet = new DocumentationSet(outputDir) {
 
             @Override
             public ApplicationDocumentList getApplicationDocumentList() {
@@ -40,8 +73,8 @@ public class Main {
             @Override
             public TranslatorList getTranslatorList() throws IOException {
                 TranslatorList translatorList = new TranslatorList();
-                translatorList.add(new BasicTranslator("NL", "nl.txt"));
-                translatorList.add(new BasicTranslator("FR", "fr.txt"));
+                translatorList.add(new BasicTranslator("NL"));
+                translatorList.add(new BasicTranslator("FR"));
                 return translatorList;
             }
 
