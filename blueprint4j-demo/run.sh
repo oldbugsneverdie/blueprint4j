40d8bdc0e582cd1531f2cee0fe199c8b9983f749
Jan <jan@blueprint4j.com>
2014-06-30 23:20:45 +0200

    No comment

---
+++ blueprint4j-demo/run.sh
@@ -0,0 +1,10 @@
+cd ../blueprint4j-core
+mvn clean install
+cd ../blueprint4j-templates
+mvn clean install
+cd ../blueprint4j-demo
+mvn clean install
+echo "================================================="
+echo "= Check the output in the ´generated´ directory ="
+echo "================================================="
+
